62c49496e4f342fc66aee1a053a79edeec8dd321
Denis Chernyatin <ya.chernyatin@gmail.com>
2023-12-06 13:29:19 +0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Denis Chernyatin on 2023.12.06.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
